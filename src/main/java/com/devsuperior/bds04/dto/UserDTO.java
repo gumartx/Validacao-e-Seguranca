02f748bcd1e25f5959b80a1bcab23ff0c75e90a6
Gustavo Gomes <gustavo.gmartx@gmail.com>
2025-02-12 14:16:39 -0300

    Configuration and UserDetails

---
+++ src/main/java/com/devsuperior/bds04/dto/UserDTO.java
@@ -0,0 +1,75 @@
+package com.devsuperior.bds04.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.devsuperior.bds04.entities.User;
+
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	@Size(min = 3, max = 20, message = "Deve conter entre 3 e 20 caracteres")
+	@NotBlank(message = "Campo requirido")
+	private String firstName;
+	@Size(min = 3, max = 20, message = "Deve conter entre 3 e 20 caracteres")
+	@NotBlank(message = "Campo requirido")
+	private String lastName;
+	@Email(message = "Email inválido")
+	private String email;
+
+	@NotEmpty(message = "Campo requirido")
+	private Set<RoleDTO> roles = new HashSet<>();
+
+	public UserDTO() {
+	}
+
+	public UserDTO(User entity) {
+		id = entity.getId();
+		email = entity.getEmail();
+		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<RoleDTO> getRoles() {
+		return roles;
+	}
+
+}
